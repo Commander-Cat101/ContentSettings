70c83e3e80bc7e3b927e0e3cfe0a220394ec810d
David Katz <dhkatz.16@gmail.com>
2024-04-12 16:45:39 -0700

    fix(build): add missing documentation to TextSettingsUI.cs

--- ContentSettings/API/Settings/DebugUI/TextSettingsUI.cs
+++ ContentSettings/API/Settings/DebugUI/TextSettingsUI.cs
@@ -12,12 +12,20 @@ using Zorro.Core;
 using Zorro.Settings;
 using Zorro.Settings.DebugUI;
 
+/// <summary>
+/// The debug UI for a text setting.
+/// </summary>
 public class TextSettingsUI : SettingUI
 {
     private TextSetting _setting;
 
     private ISettingHandler _handler;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TextSettingsUI"/> class.
+    /// </summary>
+    /// <param name="setting">The text setting to create the UI for.</param>
+    /// <param name="settingHandler">The setting handler to use.</param>
     public TextSettingsUI(TextSetting setting, ISettingHandler settingHandler)
     {
         _setting = setting;
