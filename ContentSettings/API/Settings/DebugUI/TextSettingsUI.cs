23b1d0c6700d077fcc5d8e1a4d502660b670693b
David Katz <dhkatz.16@gmail.com>
2024-04-12 16:43:17 -0700

    feat(api): support custom tabs, categories, and text input

---
+++ ContentSettings/API/Settings/DebugUI/TextSettingsUI.cs
@@ -0,0 +1,30 @@
+// -----------------------------------------------------------------------
+// <copyright file="TextSettingsUI.cs" company="ContentSettings">
+// Copyright (c) ContentSettings. All rights reserved.
+// Licensed under the GPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace ContentSettings.API.Settings.DebugUI;
+
+using UnityEngine.UIElements;
+using Zorro.Core;
+using Zorro.Settings;
+using Zorro.Settings.DebugUI;
+
+public class TextSettingsUI : SettingUI
+{
+    private TextSetting _setting;
+
+    private ISettingHandler _handler;
+
+    public TextSettingsUI(TextSetting setting, ISettingHandler settingHandler)
+    {
+        _setting = setting;
+        _handler = settingHandler;
+
+        SingletonAsset<SettingUxmls>.Instance.FloatSettingUxml.CloneTree(this);
+        var label = this.Q<Label>("SettingName");
+        label.text = setting.GetType().Name;
+    }
+}
